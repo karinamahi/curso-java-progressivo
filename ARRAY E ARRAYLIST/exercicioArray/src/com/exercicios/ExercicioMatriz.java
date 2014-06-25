a042c0567c3df54f4b86e147d8d06537a1c5dd63
Karina M H Bueno <karinamahi@hotmail.com>
2014-06-25 20:47:24 -0300

    atualizando pastas

---
+++ ARRAY E ARRAYLIST/exercicioArray/src/com/exercicios/ExercicioMatriz.java
@@ -0,0 +1,11 @@
+
+package com.exercicios;
+
+public class ExercicioMatriz {
+
+    public static void main(String[] args){
+        int loteria[] = new int[20];
+        int notas[][]=new int[5][10];
+        notas[0][0]=10;
+    }
+}
