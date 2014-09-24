f415b1efd81beef9e44c0984d89a470ffc6f78a6
Karina M H Bueno <karinamahi@hotmail.com>
2014-09-23 21:29:17 -0300

    first commit

---
+++ caixas_de_dialogo/src/br/com/caixasdedialogo/Grafico.java
@@ -0,0 +1,10 @@
+package br.com.caixasdedialogo;
+
+import javax.swing.JOptionPane;
+
+public class Grafico {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Meu primeiro programa gráfico!\nObrigada, Curso Java Progressivo!");
+	}
+}
